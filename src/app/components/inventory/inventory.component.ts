03c00d6ff299d69569ba5c54d088d83ab007fc08
Ryan <rbrenneman23@gmail.com>
2024-09-02 04:00:04 -0400

    Added empty functions to the inventory component for loading and deleting tools

--- src/app/components/inventory/inventory.component.ts
+++ src/app/components/inventory/inventory.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
   styleUrl: './inventory.component.css'
 })
-export class InventoryComponent {
+export class InventoryComponent implements OnInit {
+  
+  constructor (private router: Router) { }
 
+  ngOnInit(): void {
+    this.loadTools();
+  }
+
+  loadTools() {
+    
+  }
+
+  onDelete() {
+    
+  }
 }
