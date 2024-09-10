6876da387320aca8a492b0c828e16b7966948321
sdjordan02 <sdjordan02@gmail.com>
2024-09-10 15:18:37 -0500

    Updated the app-routing module to have the correct routing for each component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/login",
+    pathMatch: "full"
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "signup",
+    component: SignUpComponent
+  },
+  {
+    path: "inventory",
+    component: InventoryComponent
+  },
+  {
+    path: "profile",
+    component: ProfileComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
