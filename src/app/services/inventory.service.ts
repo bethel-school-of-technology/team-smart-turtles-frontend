9486b3a479c1a9ea58964a55148727789682c4f8
Ryan <rbrenneman23@gmail.com>
2024-09-12 10:12:31 -0400

    Added two operations to inventory service: updated inventory.component.ts

--- src/app/services/inventory.service.ts
+++ src/app/services/inventory.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Inventory } from '../models/inventory';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InventoryService {
+  baseURL: string = "http://localhost:3000/api";
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getAllItems(): Observable<Inventory[]> {
+    return this.http.get<Inventory[]>(this.baseURL);
+  }
+
+  deleteItem(itemId: number): Observable<any> {
+    return this.http.delete(`${this.baseURL}`)
+  }
+
+  
 }
