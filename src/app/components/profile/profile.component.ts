f5139a25a530623ebdc36a02eaa62ea4b49ffba6
Ryan <rbrenneman23@gmail.com>
2024-09-15 05:36:35 -0400

    Added loop to inventory component to display items that are added. Moved the edit button from the material toolbar to appear in the material cards containing the item information.

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
+import { Component, PLATFORM_ID, Inject, OnInit } from '@angular/core';
+import { Inventory } from '../../models/inventory';
+import { InventoryService } from '../../services/inventory.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  toolList: Inventory[] = [];
+  username: string = '';
+  email: string = '';
+
+  constructor(
+    private InventoryService: InventoryService, 
+    private router: Router, 
+    private route: ActivatedRoute, 
+    @Inject(PLATFORM_ID) private platformId: Object) {}
 
   // ngOnInit(): void {
   //   if (isPlatformBrowser(this.platformId)) {
   //     this.username = localStorage.getItem('username') || '';
   //     this.route.paramMap.subscribe(params => {
   //       this.username = params.get('username') || this.username;
-  //       this.xService.getUserPosts(this.username).subscribe(x => {
-  //         this.xList = x;
+  //       this.InventoryService.getAllItems(this.username).subscribe(t => {
+  //         this.toolList = t;
   //       }, error => {
   //         console.error('Failed to load user posts:', error);
   //         if (error.status === 404) {
@@ -24,4 +36,18 @@ export class ProfileComponent {
   //     });
   //   }
   // }
+
+  ngOnInit(): void {
+    this.InventoryService.getAllItems().subscribe(
+      (items: Inventory[]) => {
+        this.toolList = items;
+      },
+      (error: any) => {
+        console.error('Failed to load items:', error);
+        if (error.status === 404) {
+          console.error('Endpoint not found');
+        }
+      }
+    );
+  }
 }
