9a0fb12d9e8cf506457fa7da3d80aa7b5ff3d3ff
sdjordan02 <sdjordan02@gmail.com>
2024-09-18 18:13:01 -0500

    Added @angular/material command.

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -7,7 +7,7 @@ import { tap } from 'rxjs';
   providedIn: 'root'
 })
 export class UserService {
-  baseUrl: string = "http://localhost:3000/api"  //Url here
+  baseUrl: string = "http://localhost:3000/api/user/";
 
   constructor(private http: HttpClient) { }
 
