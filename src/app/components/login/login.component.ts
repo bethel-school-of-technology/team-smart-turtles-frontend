0a06e3ea228b5576491abbfead0900f656b25c93
sdjordan02 <sdjordan02@gmail.com>
2024-09-18 19:08:57 -0500

    Ryan removed throw error for ngOninIt

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -15,7 +15,7 @@ export class LoginComponent {
   constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    
   }
 
   logIn(){ 
